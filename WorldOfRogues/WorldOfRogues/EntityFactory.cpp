9df990f1443c46e8f1091410925e41d70b7f2954
emJames Hay/em <emjwhay@live.com/em>
2014-11-05 22:37:37 +0100

    Added std::cout's to EntityFactory.

--- WorldOfRogues/WorldOfRogues/EntityFactory.cpp
+++ WorldOfRogues/WorldOfRogues/EntityFactory.cpp
@@ -29,33 +29,43 @@ std::vector<BaseEntity*> EntityFactory::createEntitiesForRoom(BaseRoom *br)
 	// random amount
 	std::uniform_int_distribution<int> dist1(1, 4);
 	int amountOfEntities = dist1(dre);
+	std::cout << "Random amount of entities [" << amountOfEntities << "]" << std::endl;
 
 	// random mobs
 	for (size_t i = 0; i < amountOfEntities; i++)
 	{
 		std::uniform_int_distribution<int> dist2(1, 5);
 		int randomEntitySpawn = dist2(dre);
+		std::cout << "Random entity spawn [" << randomEntitySpawn << "]" << std::endl;
 
+		std::cout << "Created: ";
 		switch (randomEntitySpawn)
 		{
 		case 1:
+			std::cout << "Rat ";
 			entityArray.push_back(createEntity(EntityTypes::Rat));
 			break;
 		case 2:
+			std::cout << "Skeleton ";
 			entityArray.push_back(createEntity(EntityTypes::Skeleton));
 			break;
 		case 3:
+			std::cout << "Zombie ";
 			entityArray.push_back(createEntity(EntityTypes::Zombie));
 			break;
 		case 4:
+			std::cout << "Dwarf ";
 			entityArray.push_back(createEntity(EntityTypes::Dwarf));
 			break;
 		case 5:
+			std::cout << "Boss ";
 			entityArray.push_back(createEntity(EntityTypes::Boss));
 			break;
 		default:
 			break;
 		}
+
+		std::cout << std::endl;
 	}
 
 	// random entity level
@@ -64,6 +74,7 @@ std::vector<BaseEntity*> EntityFactory::createEntitiesForRoom(BaseRoom *br)
 		// change: between current player level - 2 & +3
 		std::uniform_int_distribution<int> dist3(1, 5);
 		int randomEntityLevel = dist3(dre);
+		std::cout << "Random entity level [" << randomEntityLevel << "]" << std::endl;
 
 		switch (randomEntityLevel)
 		{
