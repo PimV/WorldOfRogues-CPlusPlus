1d7c692e71a335b67c2457a7e5da3a1f87424f2a
James Hay <jwhay@live.com>
2014-11-06 21:32:13 +0100

    Randomised items

--- WorldOfRogues/WorldOfRogues/ItemFactory.cpp
+++ WorldOfRogues/WorldOfRogues/ItemFactory.cpp
@@ -27,45 +27,18 @@ std::vector<BaseItem*> ItemFactory::createRandomItems()
 	// random items
 	for (size_t i = 0; i < amountOfItems; i++)
 	{
-		std::uniform_int_distribution<int> dist2(1, 5);
+		std::uniform_int_distribution<int> dist2(0, 20);
 		int randomItemSpawn = dist2(dre);
 
-		//std::cout << "Created: ";
-		switch (randomItemSpawn)
-		{
-		case 1:
-			itemsArray.push_back(createItem(ItemType::Sword));
-			break;
-		case 2:
-			itemsArray.push_back(createItem(ItemType::Shield));
-			break;
-		case 3:
-			itemsArray.push_back(createItem(ItemType::Helmet));
-			break;
-		case 4:
-			itemsArray.push_back(createItem(ItemType::Platebody));
-			break;
-		case 5:
-			itemsArray.push_back(createItem(ItemType::Platelegs));
-			break;
-		default:
-			break;
+		if (randomItemSpawn >= 0 && randomItemSpawn <= 8) {
+			itemsArray.push_back(createItem(static_cast<WeaponType>(randomItemSpawn)));
+		}
+		else if (randomItemSpawn >= 9 && randomItemSpawn <= 20) {
+			itemsArray.push_back(createItem(static_cast<ArmourType>(randomItemSpawn)));
 		}
 
 		std::cout << std::endl;
 	}
-
-	//// random entity level
-	//for (BaseEntity *e : entityArray)
-	//{
-	//	std::uniform_int_distribution<int> dist3(1, 5);
-	//	int randomEntityLevel = dist3(dre);
-	//	std::cout << "Random entity level [" << randomEntityLevel << "]" << std::endl;
-
-	//	// change: (CurrentPlayer->getLevel() + randomEntityLevel);
-	//	e->setLevel(randomEntityLevel);
-	//}
-
 	return itemsArray;
 }
 
