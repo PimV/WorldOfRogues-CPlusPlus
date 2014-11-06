aad867d44d0112b954591292de7625f1a9dc98f0
James Hay <jwhay@live.com>
2014-11-06 17:06:53 +0100

    ItemType enum (and item string)

---
+++ WorldOfRogues/WorldOfRogues/ItemType.h
@@ -0,0 +1,18 @@
+#pragma once
+
+enum class ItemType 
+{
+	Sword = 0,
+	Shield,
+	Helmet,
+	PlateArmour,
+	PlateLegs
+};
+
+static const char* item_strings[] = {
+	"Sword",
+	"Shield",
+	"Helmet",
+	"PlateArmour",
+	"PlateLegs"
+};
